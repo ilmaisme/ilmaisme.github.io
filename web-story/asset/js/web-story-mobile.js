db019cbbd75cdd402aab51d73b0f543cef6b406c
ilmaisme <girlnamed_i@rocketmail.com>
2021-09-02 17:08:16 +0700

    rev1

--- web-story/asset/js/web-story-mobile.js
+++ web-story/asset/js/web-story-mobile.js
@@ -1,24 +1,40 @@
 $(document).ready(function () {
     /* web story slider */
-    function createSlick() {
-        $('#slide_story').slick({
+    let story = $('#slide_story');
+    $('.-js-slick-story').click(function () {
+        story.slick({
             dots: true,
             arrows: true,
             infinite: true,
-            speed: 500,
-            fade: true,
-            cssEase: 'linear',
             slidesToShow: 1,
             slidesToScroll: 1,
-            // autoplay: true,
             autoplaySpeed: 3000,
+            pauseOnHover: false,
+            pauseOnFocus: false,
             appendDots: $('#dots_story')
         });
-    }
-
-    createSlick()
-    $('.-js-slick-story').click(function () {
-        // autoplay slide when click
-        $('#slide_story').slick('slickPlay');
+        // autoplay and set slide position 
+        setTimeout(function () {
+            story.slick('slickPlay');
+            story.slick('setPosition');
+        }, 300);
+    })
+    // On before slide change
+    story.on('afterChange', function (event, slick, currentSlide) {
+        //loader dots
+        $('.slick-dots .slick-active').addClass('loaded');
+        if (slick.$slides.length == currentSlide + slick.options.slidesToScroll) {
+            //console.log("Last slide");
+            //close when last slide was hit
+            setTimeout(function () {
+                story.slick('unslick');
+                $('#pop_story').css("display", "none");
+                $('#pop_story').modal('hide');
+            }, 3100);
+        }
+    });
+    $('.storyClose').click(function () {
+        // destroy slide
+        story.slick('unslick');
     })
 })
