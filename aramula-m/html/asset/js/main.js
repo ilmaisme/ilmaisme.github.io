de4aca5cc33379edb30c0d4cab2142584fdca624
ilmaisme <girlnamed_i@rocketmail.com>
2021-10-20 11:56:06 +0700

    insight-improve

--- aramula-m/html/asset/js/main.js
+++ aramula-m/html/asset/js/main.js
@@ -1,22 +1,4 @@
 $(document).ready(function () {
-    // headline slider
-    function createSlick() {
-        $('#sheadline').slick({
-            dots: true,
-            arrows: false,
-            infinite: true,
-            speed: 500,
-            fade: true,
-            cssEase: 'linear',
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            pauseOnHover: true,
-            appendDots: $('#dtheadline')
-        });
-    }
-    createSlick();
 
     // stories slider
     function createStories() {
