b48cc8fb149c5e167a8b5ffda361b02d688f4e8e
ilmaisme <girlnamed_i@rocketmail.com>
2022-09-10 14:04:14 +0700

    grasindo homepage

--- grasindo/asset/js/main.js
+++ grasindo/asset/js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-    
+    /* sticky menu */
+    let hd = $(".header");
+    let jno = $(".js-nav-offset");
+
+    $(window).scroll(function () {
+        var a = $(window).scrollTop(),
+            b = hd.outerHeight();
+        a > b + 40 ? (hd.addClass("fixed"),
+                jno.addClass("active")) :
+            (hd.removeClass("fixed"),
+                jno.removeClass("active"))
+    })
 });
