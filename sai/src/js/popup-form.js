bc082c9fa76c7a4c5ab88dc954b6bf66c86df8b1
ilmaisme <girlnamed_i@rocketmail.com>
2022-10-10 16:47:17 +0700

    sai rev1

---
+++ sai/src/js/popup-form.js
@@ -0,0 +1,116 @@
+$(document).ready(function () {
+    //validation
+    $('#formbooking').validate({
+        rules: {
+            bookori: {
+                required: true,
+            },
+            bookdest: {
+                required: true,
+            },
+            sender: {
+                required: true,
+            },
+            receiver: {
+                required: true,
+            },
+            hpsender: {
+                required: true,
+            },
+            hpreceiver: {
+                required: true,
+            },
+            bookitem: {
+                required: true,
+            },
+            bookweight: {
+                required: true,
+            },
+        },
+        errorPlacement: function (error, element) {
+            error.insertAfter(element)
+        }
+    })
+    $('#formclaim').validate({
+        rules: {
+            claimori: {
+                required: true,
+            },
+            claimdest: {
+                required: true,
+            },
+            sender: {
+                required: true,
+            },
+            receiver: {
+                required: true,
+            },
+            hpsender: {
+                required: true,
+            },
+            hpreceiver: {
+                required: true,
+            },
+            claimitem: {
+                required: true,
+            },
+            claimweight: {
+                required: true,
+            },
+            agen: {
+                required: true,
+            },
+            datepick: {
+                required: true,
+            },
+            claimprice: {
+                required: true,
+            },
+        },
+        errorPlacement: function (error, element) {
+            error.insertAfter(element)
+        }
+    })
+
+    updatePicker()
+
+    function updatePicker() {
+        if ($('#menu').css('display') == 'none') {
+            //console.log("desktop")        
+            $('.datepick').datepicker({
+                dateFormat: 'dd/mm/yy',
+                beforeShow: function (input, inst) {
+                    var input = $('.popupWrap');
+                    // Handle calendar position before showing it.
+                    var calendar = inst.dpDiv;
+                    setTimeout(function () {
+                        calendar.position({
+                            my: 'right bottom',
+                            at: 'right bottom',
+                            collision: 'none',
+                            of: input
+                        });
+                    }, 1);
+                }
+            });
+        } else {
+            //console.log("mobile")
+            $('.datepick').datepicker({
+                dateFormat: 'dd/mm/yy',
+                beforeShow: function (input, inst) {
+                    var input = $('.popup');
+                    // Handle calendar position before showing it.
+                    var calendar = inst.dpDiv;
+                    setTimeout(function () {
+                        calendar.position({
+                            my: 'right bottom',
+                            at: 'right bottom',
+                            collision: 'none',
+                            of: input
+                        });
+                    }, 1);
+                }
+            });
+        }
+    }
+})
