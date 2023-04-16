65d995ea510414135c0934f7a5e4f1b84eb02805
ilmaisme <girlnamed_i@rocketmail.com>
2023-04-16 21:25:10 +0700

    bsnscom detail pages

---
+++ bisniscom-revamp-kanal/desktop/source/js/script-details.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('#sliderFont').on('input', function () {
+        var v = $(this).val();
+        $('.detailsContent').css('font-size', v + 'em')
+    });
+    $('.detailsFontCta.-minus').on("click", function () {
+        var minus = $('#sliderFont').attr('min')
+        $('#sliderFont').val(minus)
+        $('.detailsContent').css('font-size', minus + 'em')
+    });
+    $('.detailsFontCta.-plus').on("click", function () {
+        var plus = $('#sliderFont').attr('max')
+        $('#sliderFont').val(plus)
+        $('.detailsContent').css('font-size', plus + 'em')
+    });
+})
