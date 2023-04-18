0344a88996f1bc0914d476d957461eeb12021509
ilmaisme <girlnamed_i@rocketmail.com>
2023-04-18 10:27:59 +0700

    bsnscom detail premium

--- bisniscom-revamp-kanal/desktop/source/js/script-details.js
+++ bisniscom-revamp-kanal/desktop/source/js/script-details.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //font details slider range custom
     $('#sliderFont').on('input', function () {
         var v = $(this).val();
         $('.detailsContent').css('font-size', v + 'em')
@@ -14,3 +15,26 @@ $(document).ready(function () {
         $('.detailsContent').css('font-size', plus + 'em')
     });
 })
+
+//share link article
+let share = document.getElementById('share');
+share.addEventListener('click', function (e) {
+    // copy text
+    //console.log(e.target)
+    if (e.target.matches('[data-form=copyLinkUrl]')) {
+        e.preventDefault()
+        copyLink('copyLinkUrl', e)
+    }
+});
+
+function copyLink(id, e) {
+    let cTxt = document.getElementById(id);
+    
+    cTxt.select();
+    cTxt.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(cTxt.value);
+    e.target.classList.add('-tooltip');
+    setTimeout(function () {
+        e.target.classList.remove('-tooltip');
+    }, 1500);
+}
