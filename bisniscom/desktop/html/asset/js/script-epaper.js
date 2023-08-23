c906a6e0858773ff5027ccf9a8b72a2a40f1ad01
ilmaisme <girlnamed_i@rocketmail.com>
2023-08-23 21:17:55 +0700

    update port

--- bisniscom/desktop/html/asset/js/script-epaper.js
+++
@@ -1,5 +0,0 @@
-$(".-js-close").on("click", function () {
-    let parent = $(this).parent().parent();
-    parent.fadeOut("fast");
-    $("#back-to-top").css('bottom', '25px');
-})
--- bisniscom/mobile/html/asset/js/flashnews.js
+++
@@ -1,9 +0,0 @@
-$(document).scroll(function () {
-    var moveScroll = $(this).scrollTop();
-    // if (moveScroll > 100) {
-    if ($("#sticky1-sticky-wrapper").hasClass("is-sticky")) {
-        $(".wrapper__flashnews").addClass("bottom");
-    } else {
-        $(".wrapper__flashnews").removeClass("bottom");
-    }
-});
