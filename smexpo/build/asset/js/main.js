ef4cedb3cc37565e9e8d40b9eebd85683cb1fd7e
Ilma A <girlnamed_i@rocketmail.com>
2023-10-02 14:58:28 +0700

    smexpo header m

--- smexpo/build/asset/js/main.js
+++ smexpo/build/asset/js/main.js
@@ -1,3 +1,79 @@
-$(document).ready(function () {
-    
+let hd = $(".header"),
+    mTg = $('#menuToggle');
+
+uncheckedMenu()
+hideMenuMobile()
+updateMenu()
+if (window.history && window.history.pushState) {
+    window.history.pushState('forward', null, '');
+    $(window).on('popstate', function () {
+        uncheckedMenu()
+        hideMenuMobile()
+    });
+}
+
+$(window).on('resize', function () {
+    updateMenu()
+    if (viewport().width >= 1230) {
+        uncheckedMenu()
+        hideMenuMobile()
+    }
 });
+
+$(document).ready(function () {
+    //toggle mobile menu
+    mTg.on('click', function () {
+        isChecked = $(this).is(':checked')
+
+        if (isChecked) {
+            $('.menuMob').addClass('active');
+            $('.menu').addClass('active');
+        } else {
+            hideMenuMobile()
+        }
+    })
+});
+
+//calc width window
+function viewport() {
+    var e = window,
+        a = 'inner';
+    if (!('innerWidth' in window)) {
+        a = 'client';
+        e = document.documentElement || document.body;
+    }
+    return {
+        width: e[a + 'Width'],
+        height: e[a + 'Height']
+    };
+}
+
+//update mobile menu
+function updateMenu() {
+    if (viewport().width >= 1230) {
+        // console.log("desktop")
+        $('.menuMob').removeClass('mobile');
+        hd.removeClass('mobile');
+        /* move nav position in desktop */
+        $('.nav').appendTo('#navDesk');
+        $('.headerAccess').appendTo('#accessDesk');
+        $('.searchbox').appendTo('.searchboxWrap');
+    } else {
+        // console.log("mobile")
+        $('.menuMob').addClass('mobile');
+        hd.addClass('mobile');
+        /* move nav position in mobile */
+        $('.nav').appendTo('#navMob');
+        $('.headerAccess').appendTo('#accessMob');
+        $('.searchbox').appendTo('.menuMob');
+    }
+}
+
+function uncheckedMenu() {
+    mTg.prop('checked', false);
+}
+
+function hideMenuMobile() {
+    $('.menuMob').removeClass('active');
+    $('.menu').removeClass('active');
+}
