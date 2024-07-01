e18ffc85a5a7f5a3956f7c901065debe811a0026
Ilma A <girlnamed_i@rocketmail.com>
2024-07-01 12:11:53 +0700

    homepg photo

--- bisniscom-revamp-kanal/desktop/source/js/script-home.js
+++ bisniscom-revamp-kanal/desktop/source/js/script-home.js
@@ -66,6 +66,33 @@ $(document).ready(function () {
             // autoplay: 1,
         });
     }
+
+    if ($('.-js-photo-slider').length > 0) {
+        // slider photo
+        $('.-js-photo-slider').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            swipe: false,
+            infinite: true,
+            autoplay: true,
+            speed: 1000,
+            autoplaySpeed: 2000,
+            asNavFor: '.-js-photo-slider-nav',
+        });
+        $('.-js-photo-slider-nav').slick({
+            dots: false,
+            arrows: false,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            infinite: true,
+            focusOnSelect: true,
+            centerMode: false,
+            speed: 1000,
+            asNavFor: '.-js-photo-slider',
+        });
+    }
 })
 
 //cta trigger active
