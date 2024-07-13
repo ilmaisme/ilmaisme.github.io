965c692815b16007081cefe003804da80e6021a5
Ilma A <girlnamed_i@rocketmail.com>
2024-07-13 10:32:33 +0700

    mobile compo4

--- bisniscom-revamp-kanal/mobile/html/asset/js/script-home.js
+++ bisniscom-revamp-kanal/mobile/html/asset/js/script-home.js
@@ -11,6 +11,47 @@ $(document).ready(function () {
             // autoplay: 1,
         });
     }
+
+    if ($('.-js-igrafis').length > 0) {
+        // slider infografis
+        $('.-js-igrafis').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            swipe: true,
+            infinite: true,
+            // autoplay: 1,
+        });
+    }
+
+    if ($('.-js-photo-slider').length > 0) {
+        // slider photo
+        $('.-js-photo-slider').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            swipe: true,
+            infinite: true,
+            autoplay: true,
+            speed: 1000,
+            autoplaySpeed: 2000,
+            // accessibility: false,
+            asNavFor: '.-js-photo-slider-nav',
+        });
+        $('.-js-photo-slider-nav').slick({
+            dots: false,
+            arrows: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            focusOnSelect: true,
+            centerMode: false,
+            speed: 1000,
+            // accessibility: false,
+            asNavFor: '.-js-photo-slider',
+        });
+    }
 })
 
 //cta trigger active
