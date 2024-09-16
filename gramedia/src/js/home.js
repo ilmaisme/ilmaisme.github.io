b3e41772bf036b2c6cb98a23861c52179f318d98
Ilma A <girlnamed_i@rocketmail.com>
2024-09-17 00:35:21 +0700

    gramed home -d

--- gramedia/src/js/home.js
+++ gramedia/src/js/home.js
@@ -1,6 +1,129 @@
 $(document).ready(function () {
     //headline logo
     initTransformLogo();
+
+    //slider new release
+    $('.-supdates').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        mobileFirst: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        responsive: [{
+                breakpoint: 1441,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 1020,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+    //slider brands
+    $('.-sbrands').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 700,
+        mobileFirst: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+    //slider testimonial
+    $('.-stesti').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 2000,
+        mobileFirst: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [{
+                breakpoint: 1441,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 1020,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                }
+            }
+        ]
+    });
+
+    //slider widget event
+    $('.-sEventWid').on('afterChange init', function (event, slick, direction) {
+            // console.log('afterChange/init', event, slick, slick.$slides);
+            // remove all prev/next
+            slick.$slides.removeClass('prevSlide').removeClass('nextSlide');
+
+            // find current slide
+            for (var i = 0; i < slick.$slides.length; i++) {
+                var $slide = $(slick.$slides[i]);
+                if ($slide.hasClass('slick-current')) {
+                    // update DOM siblings
+                    $slide.prev().addClass('prevSlide');
+                    $slide.next().addClass('nextSlide');
+                    break;
+                }
+            }
+        })
+        .on('beforeChange', function (event, slick) {
+            // remove all prev/next
+            slick.$slides.removeClass('prevSlide').removeClass('nextSlide');
+        })
+    $('.-sEventWid').slick({
+        // centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        vertical: true,
+        verticalSwiping: true,
+        arrows: false,
+        swipeToSlide: true,
+        // autoplay: true
+    });
 });
 
 function initTransformLogo() {
@@ -11,7 +134,7 @@ function initTransformLogo() {
 }
 
 function initAnimateTransform() {
-    console.log('animate');
+    //console.log('animate');
     zSequence = 0;
     setTimeout(function () {
         $('.g-2').fadeIn();
--- gramedia/src/js/main.js
+++ gramedia/src/js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
-    
+    let hd = $(".header");
+
+    // sticky menu
+    $(window).scroll(function () {
+        var a = $(window).scrollTop(),
+            b = hd.outerHeight();
+        a > b + 0 ? (hd.addClass("fixed")) :
+            (hd.removeClass("fixed"))
+    })
 });
+
+/* s: Get HEIGHT Device */
+const appHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+}
+window.addEventListener("resize", appHeight)
+appHeight()
+/* e: Get HEIGHT Device */
