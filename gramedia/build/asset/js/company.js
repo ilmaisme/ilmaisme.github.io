821c88f5458e2f23d82b715433eb8a824f9428a9
Ilma A <girlnamed_i@rocketmail.com>
2024-09-20 17:35:38 +0700

    gramed company

---
+++ gramedia/build/asset/js/company.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    $('.-sawards').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 700,
+        mobileFirst: true,
+        centerMode: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        swipeToSlide: true,
+        centerPadding: '50px',
+        responsive: [{
+            breakpoint: 1441,
+            settings: {
+                slidesToShow: 5,
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 700,
+            centerPadding: '80px',
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+            }
+        }
+    ]
+    });
+});
--- gramedia/src/js/home.js
+++ gramedia/src/js/home.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-    //headline logo
-    initTransformLogo();
 
     //slider new release
     $('.-supdates').slick({
@@ -14,19 +12,19 @@ $(document).ready(function () {
         autoplay: false,
         autoplaySpeed: 2000,
         responsive: [{
-                breakpoint: 1441,
+                breakpoint: 1700,
                 settings: {
                     slidesToShow: 4,
                 }
             },
             {
-                breakpoint: 1020,
+                breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
                 }
             },
             {
-                breakpoint: 700,
+                breakpoint: 820,
                 settings: {
                     slidesToShow: 2,
                 }
@@ -62,7 +60,7 @@ $(document).ready(function () {
         infinite: true,
         speed: 2000,
         mobileFirst: true,
-        slidesToShow: 2,
+        slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
@@ -73,9 +71,9 @@ $(document).ready(function () {
                 }
             },
             {
-                breakpoint: 1020,
+                breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 2,
                 }
             },
             {
@@ -87,7 +85,7 @@ $(document).ready(function () {
             {
                 breakpoint: 480,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 1,
                 }
             }
         ]
@@ -125,194 +123,3 @@ $(document).ready(function () {
         // autoplay: true
     });
 });
-
-function initTransformLogo() {
-    setTimeout(function () {
-        $('.g-1').fadeIn();
-        initAnimateTransform();
-    }, 500);
-}
-
-function initAnimateTransform() {
-    //console.log('animate');
-    zSequence = 0;
-    setTimeout(function () {
-        $('.g-2').fadeIn();
-        zSequence = zSequence + 1;
-        $('.g-2').css({
-            '-webkit-animation': 'scale 1.5s',
-            'z-index': zSequence
-        });
-        // loopOngoing = true;
-    }, 1000);
-    setTimeout(function () {
-        $('.g-1').fadeOut();
-    }, 2000);
-    setTimeout(function () {
-        $('.g-1').fadeIn();
-        zSequence = zSequence + 1;
-        $('.g-1').css({
-            '-webkit-animation': 'scale 1.5s',
-            'z-index': zSequence
-        });
-    }, 3000);
-    setTimeout(function () {
-        $('.g-2').fadeOut();
-    }, 4000);
-    setTimeout(function () {
-        $('.g-3').fadeIn();
-        zSequence = zSequence + 1;
-        $('.g-3').css({
-            '-webkit-animation': 'scale 1.5s',
-            'z-index': zSequence
-        });
-    }, 5000);
-    setTimeout(function () {
-        $('.g-1').fadeOut();
-    }, 6000);
-    setTimeout(function () {
-        $('.g-1').fadeIn();
-        zSequence = zSequence + 1;
-        $('.g-1').css({
-            '-webkit-animation': 'scale 1.5s',
-            'z-index': zSequence
-        });
-    }, 7000);
-    setTimeout(function () {
-        $('.g-3').fadeOut();
-    }, 8000);
-    setTimeout(function () {
-        $('.g-4').fadeIn();
-        zSequence = zSequence + 1;
-        $('.g-4').css({
-            '-webkit-animation': 'scale 1.5s',
-            'z-index': zSequence
-        });
-    }, 9000);
-    setTimeout(function () {
-        $('.g-1').fadeOut();
-    }, 10000);
-    setTimeout(function () {
-        $('.g-1').fadeIn();
-        zSequence = zSequence + 1;
-        $('.g-1').css({
-            '-webkit-animation': 'scale 1.5s',
-            'z-index': zSequence
-        });
-    }, 11000);
-    setTimeout(function () {
-        $('.g-4').fadeOut();
-    }, 12000);
-    setTimeout(function () {
-        $('.g-5').fadeIn();
-        zSequence = zSequence + 1;
-        $('.g-5').css({
-            '-webkit-animation': 'scale 1.5s',
-            'z-index': zSequence
-        });
-    }, 13000);
-    setTimeout(function () {
-        $('.g-1').fadeOut();
-    }, 14000);
-    setTimeout(function () {
-        $('.g-1').fadeIn();
-        zSequence = zSequence + 1;
-        $('.g-1').css({
-            '-webkit-animation': 'scale 1.5s',
-            'z-index': zSequence
-        });
-    }, 15000);
-    setTimeout(function () {
-        $('.g-5').fadeOut();
-    }, 16000);
-    setTimeout(function () {
-        $('.g-6').fadeIn();
-        zSequence = zSequence + 1;
-        $('.g-6').css({
-            '-webkit-animation': 'scale 1.5s',
-            'z-index': zSequence
-        });
-    }, 17000);
-    setTimeout(function () {
-        $('.g-1').fadeOut();
-    }, 18000);
-    setTimeout(function () {
-        $('.g-1').fadeIn();
-        zSequence = zSequence + 1;
-        $('.g-1').css({
-            '-webkit-animation': 'scale 1.5s',
-            'z-index': zSequence
-        });
-    }, 19000);
-    setTimeout(function () {
-        $('.g-6').fadeOut();
-    }, 20000);
-    setTimeout(function () {
-        $('.g-7').fadeIn();
-        zSequence = zSequence + 1;
-        $('.g-7').css({
-            '-webkit-animation': 'scale 1.5s',
-            'z-index': zSequence
-        });
-    }, 21000);
-    setTimeout(function () {
-        $('.g-1').fadeOut();
-    }, 22000);
-    setTimeout(function () {
-        $('.g-1').fadeIn();
-        zSequence = zSequence + 1;
-        $('.g-1').css({
-            '-webkit-animation': 'scale 1.5s',
-            'z-index': zSequence
-        });
-    }, 23000);
-    setTimeout(function () {
-        $('.g-7').fadeOut();
-    }, 24000);
-    setTimeout(function () {
-        $('.g-8').fadeIn();
-        zSequence = zSequence + 1;
-        $('.g-8').css({
-            '-webkit-animation': 'scale 1.5s',
-            'z-index': zSequence
-        });
-    }, 25000);
-    setTimeout(function () {
-        $('.g-1').fadeOut();
-    }, 26000);
-    setTimeout(function () {
-        $('.g-1').fadeIn();
-        zSequence = zSequence + 1;
-        $('.g-1').css({
-            '-webkit-animation': 'scale 1.5s',
-            'z-index': zSequence
-        });
-    }, 27000);
-    setTimeout(function () {
-        $('.g-8').fadeOut();
-    }, 28000);
-    setTimeout(function () {
-        $('.g-9').fadeIn();
-        zSequence = zSequence + 1;
-        $('.g-9').css({
-            '-webkit-animation': 'scale 1.5s',
-            'z-index': zSequence
-        });
-    }, 29000);
-    setTimeout(function () {
-        $('.g-1').fadeOut();
-    }, 30000);
-    setTimeout(function () {
-        $('.g-1').fadeIn();
-        zSequence = zSequence + 1;
-        $('.g-1').css({
-            '-webkit-animation': 'scale 1.5s',
-            'z-index': zSequence
-        });
-    }, 31000);
-    setTimeout(function () {
-        $('.g-9').fadeOut();
-        loopOngoing = false;
-        initAnimateTransform();
-    }, 32000);
-}
