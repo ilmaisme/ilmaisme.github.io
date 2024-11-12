f1db65f58bbc49c633edf38ef457fbea1232ed4d
Ilma A <girlnamed_i@rocketmail.com>
2024-11-12 20:38:38 +0700

    elex categ

--- elex/build/asset/js/category.js
+++ elex/build/asset/js/category.js
@@ -1,15 +1,31 @@
 $(document).ready(function () {
     var swiper = new Swiper(".categoryHl__nav", {
         loop: true,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        speed: 1000,
         slidesPerView: 4,
         freeMode: true,
-        direction: 'vertical',
+        direction: 'horizontal',
         watchSlidesProgress: true,
+        breakpoints: {
+            1230: {
+                direction: 'vertical'
+            }
+        }
     });
     var swiper2 = new Swiper(".categoryHl__big", {
         loop: true,
+        effect: "fade",
         thumbs: {
             swiper: swiper,
         },
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        speed: 1000,
     });
 });
