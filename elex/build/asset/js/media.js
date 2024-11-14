0202902b88a07123530aa1d60fa83587eb486e72
Ilma A <girlnamed_i@rocketmail.com>
2024-11-14 10:30:20 +0700

    elex photo init

--- elex/build/asset/js/media.js
+++ elex/build/asset/js/media.js
@@ -18,6 +18,79 @@ $(document).ready(function () {
         }
         triggerActiveButton('.-pVideo')
     });
+
+    //photoswipe init
+    var openPhotoSwipe = function (elTitle, elImg) {
+        var pswpElement = document.querySelectorAll('.pswp')[0];
+
+        // build items array
+        var items = [{
+                src: elImg,
+                title: elTitle,
+                w: 980,
+                h: 550,
+            },
+            {
+                src: 'https://picsum.photos/id/192/980/550',
+                title: 'Bread ugh tilde diy bag big hot unicorn 90\'s trade. Church-key adaptogen raclette pbr&b four food gastropub heard migas. Vape four dreamcatcher literally.',
+                w: 980,
+                h: 550,
+            },
+            {
+                src: 'https://picsum.photos/id/163/980/550',
+                title: 'Bread ugh tilde diy bag big hot unicorn 90\'s trade. Church-key adaptogen raclette pbr&b four food gastropub heard migas. Vape four dreamcatcher literally.',
+                w: 980,
+                h: 550,
+            },
+            {
+                src: 'https://picsum.photos/id/180/980/550',
+                title: 'Bread ugh tilde diy bag big hot unicorn 90\'s trade. Church-key adaptogen raclette pbr&b four food gastropub heard migas. Vape four dreamcatcher literally.',
+                w: 980,
+                h: 550,
+            },
+            {
+                src: 'https://picsum.photos/id/119/980/550',
+                title: 'Bread ugh tilde diy bag big hot unicorn 90\'s trade. Church-key adaptogen raclette pbr&b four food gastropub heard migas. Vape four dreamcatcher literally.',
+                w: 980,
+                h: 550,
+            }
+        ];
+
+        // define options (if needed)
+        options = {
+
+            closeOnScroll: false,
+            fullscreenEl: false,
+            shareEl: false,
+            minZoom: 3,
+            fullscreenEl: false,
+            zoomEl: false,
+            indexIndicator: false,
+            initialZoomLevel: 1,
+            // secondaryZoomLevel: 1.5,
+            // maxZoomLevel: 1,
+            history: false,
+            addCaptionHTMLFn: function (item, captionEl /*, isFake */ ) {
+                if (!item.title) {
+                    captionEl.children[0].innerHTML = '';
+                    return false;
+                }
+                let _1vh = $(window).height();
+                captionEl.children[0].innerHTML = item.title;
+                return true;
+            }
+
+        };
+
+        var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
+    };
+
+    $('.-photoswipeTrig').click(function () {
+        var title = $(this).find('.mediaTitle').text(),
+            img = $(this).find('.mediaImg img').attr('src')
+        openPhotoSwipe(title, img)
+    });
 });
 
 //pause video
