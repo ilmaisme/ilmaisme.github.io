66cf7ec046b0e21297ac61ab3f4b12d11795a9be
Ilma A <girlnamed_i@rocketmail.com>
2025-05-29 21:46:24 +0700

    kontemporer update

--- kontemporer/desktop/source/main.js
+++ kontemporer/desktop/source/main.js
@@ -1,23 +1,4 @@
 $(document).ready(function () {
-    // headline slider
-    function createSlick() {
-        $('#sheadline').slick({
-            dots: true,
-            arrows: false,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            pauseOnHover: true,
-            appendDots: $('#dtheadline')
-        });
-    }
-    if ($('#sheadline').is(':visible')) {
-        createSlick();
-    }
-
     // box slider
     function createBox() {
         $('#sbox').slick({
