e90f48423d1f79fb71fba6433befb5e689e641b2
Ilma A <girlnamed_i@rocketmail.com>
2025-06-09 11:05:17 +0700

    kontem m init

---
+++ kontemporer/mobile/html/asset/js/global.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+    
+    // sticky menu
+    let hd = $("header");
+    let hdl = $(".headerLogo");
+
+    $(window).scroll(function () {
+        var a = $(window).scrollTop(),
+            b = hd.outerHeight();
+        a > b + 40 ? (hd.addClass("active"),
+                hdl.addClass("scale")) :
+            (hd.removeClass("active"),
+                hdl.removeClass("scale"))
+    })
+    
+    //close sticky ads
+    $('.-closeads').click(function (e) {
+        e.preventDefault();
+        $(this).parent().remove();
+    });
+
+    //scroll menu nav
+    let nav = $('.navLink.active');
+    if (nav.length > 0) {
+        let msp = nav.position().left - nav.width();
+        $('.navList').animate({
+            scrollLeft: msp
+        });
+    }
+
+    //close popup
+    $('.-btnCl').on("click", function () {
+        $('.popup').removeClass('active');
+    });
+
+
+    //icon play trigger
+    if ($('.videoPlay').is(':visible')) {
+        $('.videoPlay .icon-play').click(function () {
+            if ($('.videoPlay video').paused == false) {
+                $('.videoPlay video').get(0).pause();
+            } else {
+                $('.videoPlay video').get(0).play();
+            }
+        });
+        $(".videoPlay video").on("play", function () {
+            $('.icon-play').hide();
+        });
+        $(".videoPlay video").on("pause", function () {
+            $('.icon-play').show();
+        });
+    }
+
+})
+
+function backToTop() {
+    // $(window).scrollTop(0);
+    $('html,body').stop().animate({
+        scrollTop: 0
+    }, 1000);
+}
+
+//header menu scroll
+function updateNavListAlignment() {
+  const navLists = document.querySelectorAll('.navList');
+
+  navLists.forEach(navList => {
+    const container = navList.closest('.nav');
+    if (!container) return;
+
+    if (navList.scrollWidth > container.clientWidth) {
+      navList.classList.add('is-overflowing');
+    } else {
+      navList.classList.remove('is-overflowing');
+    }
+  });
+}
+
+window.addEventListener('load', updateNavListAlignment);
+window.addEventListener('resize', updateNavListAlignment);
