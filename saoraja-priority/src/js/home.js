71cdfa93d7490f0cff37e522ee65904b8219d1a7
Ilma A <girlnamed_i@rocketmail.com>
2025-07-30 02:12:25 +0700

    saoraja nav

--- saoraja-priority/src/js/home.js
+++ saoraja-priority/src/js/home.js
@@ -1,35 +1,24 @@
 $(document).ready(function () {
     //slider widget event
-    var swiper = new Swiper(".-sEventWid", {
-        direction: 'vertical',
-        effect: "coverflow",
-        grabCursor: false,
+    var swiper = new Swiper(".-sHL", {
+        grabCursor: true,
         loop: true,
         speed: 1000,
-        centeredSlides: true,
-        centeredSlidesBounds : true,
-        slidesPerView: "auto",
-        loopFillGroupWithBlank: true,
-        coverflowEffect: {
-            rotate: 1, // Slide rotate in degrees
-            stretch: 283, // Stretch space between slides (in px)
-            depth: 40, // Depth offset in px (slides translate in Z axis)
-            modifier: 1, // Effect multipler
-            scale: .95,
-            slideShadows: false, // Enables slides shadows
+        effect: "fade",
+        autoplay: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
         },
-        autoplay: false,
-        parallax: true,
-        reverseDirection: true,
-        breakpoints: {
-            640: {
-                coverflowEffect: {
-                    rotate: 1,
-                    modifier: 1,
-                    stretch: 280,
-                    depth: 40,
-                }
-            }
-        }
+        // breakpoints: {
+        //     640: {
+        //         coverflowEffect: {
+        //             rotate: 1,
+        //             modifier: 1,
+        //             stretch: 280,
+        //             depth: 40,
+        //         }
+        //     }
+        // }
     });
 });
