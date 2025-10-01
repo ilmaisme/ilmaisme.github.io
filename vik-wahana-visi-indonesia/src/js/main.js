dc6252902ae7d844374c9202494f04ff8da3f014
Ilma A <girlnamed_i@rocketmail.com>
2025-10-01 12:39:39 +0700

    vik fix height

--- vik-wahana-visi-indonesia/src/js/main.js
+++ vik-wahana-visi-indonesia/src/js/main.js
@@ -8,12 +8,12 @@ function setAppHeight() {
 
 setAppHeight();
 
-if (window.visualViewport) {
-  window.visualViewport.addEventListener("resize", setAppHeight);
-  window.visualViewport.addEventListener("scroll", setAppHeight);
-} else {
-  window.addEventListener("resize", setAppHeight);
-}
+// if (window.visualViewport) {
+//   window.visualViewport.addEventListener("resize", setAppHeight);
+//   window.visualViewport.addEventListener("scroll", setAppHeight);
+// } else {
+//   window.addEventListener("resize", setAppHeight);
+// }
 /* e: Get HEIGHT Device */
 
 //-- Language toggle switch --//
@@ -24,10 +24,10 @@ const switchTxt = document.querySelector(".switchTxt");
 const pathParts = window.location.pathname.split("/");
 const currentFile = pathParts.pop() || "index.html";
 
-// const basePathEN = "/en/";
-// const basePathID = "/";
-const basePathEN = "https://ilmaisme.github.io/vik-wahana-visi-indonesia/build/en/";
-const basePathID = "https://ilmaisme.github.io/vik-wahana-visi-indonesia/build/";
+const basePathEN = "/en/";
+const basePathID = "/";
+// const basePathEN = "https://ilmaisme.github.io/vik-wahana-visi-indonesia/build/en/";
+// const basePathID = "https://ilmaisme.github.io/vik-wahana-visi-indonesia/build/";
 const isEnglishPage = pathParts.includes("en");
 
 // --- On load, restore from localStorage ---
